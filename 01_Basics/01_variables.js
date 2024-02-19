0b81fc5ada1215b922ebfa307a0b893a33089b7b
532manojc <138588682+532manojc@users.noreply.github.com>
2024-02-19 16:44:07 +0000

    basic of variables done

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,12 @@
+const accId='bsgs'
+let accEmail=53453
+var accPassword=123459
+accCity="mumbai"
+
+
+console.log(accId);
+console.log(accPassword);
+console.log(accEmail);
+console.log(accCity);
+
+console.table([accId,accEmail,accPassword,accCity])
