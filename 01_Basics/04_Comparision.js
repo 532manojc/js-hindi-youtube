6b9ea308a33790aa3c734f394a7bb0cf170216b3
532manojc <138588682+532manojc@users.noreply.github.com>
2024-02-27 14:26:21 +0000

    comparsion of datatypes

---
+++ 01_Basics/04_Comparision.js
@@ -0,0 +1,21 @@
+console.log(2 > 1);
+console.log(2 ==1);
+console.log(2 < 1);
+console.log(2 != 1);
+console.log(2 === 1);  
+
+console.log("2" >1);  //avoid
+console.log("02" >1);    //avoid
+
+console.log(null > 0);    //avoid
+console.log(null == 0);    //avoid
+console.log(null >= 0);    //avoid
+
+console.log(undefined > 0);    //avoid
+console.log(undefined == 0);    //avoid
+console.log(undefined >= 0);    //avoid
+
+//  ===
+
+console.log("2" === 2);
+console.log(2 === 2);
