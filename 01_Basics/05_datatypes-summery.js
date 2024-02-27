1d0bc9bdc33737a45adfcc1b7411f2240459952f
532manojc <138588682+532manojc@users.noreply.github.com>
2024-02-27 16:32:02 +0000

    Data type symmery

---
+++ 01_Basics/05_datatypes-summery.js
@@ -0,0 +1,17 @@
+const id=Symbol('123')
+const anotherId=Symbol('123')
+
+console.log(id === anotherId);
+
+const heros=["iron man","hulk","thor","loki"];  //array
+console.log(heros);
+
+let myObj={         //object decleration
+  name:"manoj",
+  age:22,
+}
+console.log(myObj);
+
+const myfunction=function(){   //function decleration
+  console.log("hello world");
+}
