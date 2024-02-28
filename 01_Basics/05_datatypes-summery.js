844b59c7b042b2020812ee91a84a0221e7a2edf9
532manojc <138588682+532manojc@users.noreply.github.com>
2024-02-28 12:44:13 +0000

    memory in JS

--- 01_Basics/05_datatypes-summery.js
+++ 01_Basics/05_datatypes-summery.js
@@ -15,3 +15,28 @@ console.log(myObj);
 const myfunction=function(){   //function decleration
   console.log("hello world");
 }
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+//stack -> premitive   ------creat a copy of original data and work on it
+//heap -> non-premitive    ------work by reference ,this means any change made on parameter will affect the original value
+
+let myYoutubeName="hitesh choudary"
+
+let anotherName=myYoutubeName
+anotherName="chaiaurcode";
+
+console.log(myYoutubeName);
+console.log(anotherName);
+
+let userOne ={
+  email:"user1@gmail.com",
+  upi:"user1@ybl",
+}
+
+let userTwo=userOne;
+
+userTwo.email="user2@gmail.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
